ac74a49794018179da96c529611e0d382d619754
Nazar Andrushko <nazar.andrushko@litslink.com>
2023-08-10 14:07:35 +0300

    feat: add tool for distance and nearby homes. fix: change llm version to a chatgpt-16k. feat: add 2 previous messages as context to a model. feat: add refinement chatgpt call

--- ai_model.py
+++ ai_model.py
@@ -1,21 +1,49 @@
-import requests
-from typing import Optional, Type
-from langchain.tools.base import StructuredTool
 from langchain.prompts import MessagesPlaceholder
 from langchain.memory import ConversationBufferMemory
 from langchain.agents import initialize_agent, AgentType
 from langchain.chat_models import ChatAnthropic
 from langchain.chat_models import ChatOpenAI
+from langchain import LLMChain
+from langchain.schema import AIMessage, HumanMessage, SystemMessage
+from langchain.agents import ZeroShotAgent, Tool, AgentExecutor
+from langchain.memory import ConversationBufferMemory
+from langchain import OpenAI, LLMChain
+from langchain.utilities import GoogleSearchAPIWrapper
+from human_tool import HumanInputRun
+from pydantic import BaseModel, Field
+import requests
+from langchain.tools import StructuredTool
+from typing import Optional
+import openai
+from langchain.tools import GooglePlacesTool
+import os
+import pickle
+from langchain.memory.chat_message_histories import FileChatMessageHistory
+from langchain.utilities.google_places_api import GooglePlacesAPIWrapper
+import re
+import googlemaps
+import langchain
+import os
+from dotenv import load_dotenv
+
+# Load .env file
+load_dotenv()
+
+# Read an environment variable
+os.environ["GPLACES_API_KEY"] =  os.getenv('GPLACES_API_KEY')
+
+
+
 
 def find_zpid(
     location: Optional[str] = None, 
 ) -> dict:
-    """Tool that uses Zillow api to find zpid given adress of the house. Use case find zpid when someone is interested in house. Valid params include "location":"location"."""
+    """Tool that uses Zillow api to find zpid given adress of the house. Use case find zpid when need to use get_house_property tool. Valid params include "location":"location"."""
 
     base_url = "https://zillow-com1.p.rapidapi.com/propertyExtendedSearch"
     
     headers = {
-	"X-RapidAPI-Key": "cad9458103mshe64bf73cf4954ebp1be8eajsn95afe237a166",
+	"X-RapidAPI-Key": os.getenv('X-RapidAPI-Key'),
 	"X-RapidAPI-Host": "zillow-com1.p.rapidapi.com"
     }
 
@@ -25,65 +53,229 @@ def find_zpid(
         querystring['location'] = location
         
 
-    print(f'Search with {querystring}')
+    # print(f'Search with {querystring}')
     result = requests.get(base_url, params=querystring, headers=headers)
-    return result.json()
-
-
+    print(f'ZPID = {result.json()}')
+    try :
+        if isinstance(result.json(),list):
+            return result.json()[0]["zpid"]
+        return result.json()["zpid"]
+    except :
+        print('EXCEPTION')
+        return "Sorry, could you please give full address"
 
 def post_process_house_property(house_property):
-    house_property.pop('nearbyHomes')
+    # house_property.pop('nearbyHomes')
     house_property.pop('listed_by')
     house_property.pop('priceHistory')
     house_property.pop('resoFacts')
+    house_property.pop('taxHistory') 
+    # house_property.pop('mlsDisclaimer')
+    
+    print(house_property)
     return house_property
 
 def get_house_property(
-    zpid: Optional[int] = None, 
+    location: Optional[str] = None, 
 ) -> dict:
-    """Tool that uses Zillow api to get information about house or it's neighborhood given zpid of the house.Use case answer on questions related to the house or it's neighborhood. Valid params include "zpid":"zpid"."""
+    """Tool that uses Zillow api to get house properties given adress of the house.Use case answer on questions related to the house. Valid params include "location":"location"."""
 
+    zpid = find_zpid(location)
+    if zpid == "Sorry, could you please give full address":
+        return zpid
+    
     base_url = "https://zillow-com1.p.rapidapi.com/property"
     
     headers = {
-	"X-RapidAPI-Key": "cad9458103mshe64bf73cf4954ebp1be8eajsn95afe237a166",
+	"X-RapidAPI-Key": os.getenv('X-RapidAPI-Key'),
 	"X-RapidAPI-Host": "zillow-com1.p.rapidapi.com"
     }
-    
+
     if zpid is not None:
         querystring = {"zpid":f"{zpid}"}
     else:
         raise Exception("Didn't get zpid")
         
 
+    # print(f'Search with {querystring}')
     result = requests.get(base_url, params=querystring, headers=headers)
+    # print(result.json())
     post_processed = post_process_house_property(result.json())
     return post_processed
 
+def get_quesion(x: Optional[str] = None):
+    return x
+
+
+def find_distance(addresses:str) -> str:
+    '''Find distance tool, useful when need to find distance between two exact addresses'''
+    print("FIND DISTANCE FUNCTION")
+    address1, address2 = addresses.split('|')
+    print(address1,address2)
+    address_regex = '\d+\s[A-Za-z0-9\s]+\,\s[A-Za-z\s]+\,\s[A-Z]{2}\s\d{5}'
+    match = re.search(address_regex, address2)
+    if not match:
+        address2 = GooglePlacesTool(api_wrapper=GooglePlacesAPIWrapper(top_k_results=1)).run(f'{address2} near {address1}').split('Address:')[1].split('\n')[0]
+    print('distance between', address2, address1)
+    gmaps = googlemaps.Client(key='AIzaSyAuj7gPxOpEWM6V6ckw0aErmR5FKS1-poI')
+    data = gmaps.distance_matrix(address1,address2)
+    my_dist =data['rows'][0]['elements'][0]
+    if my_dist['status'] == 'NOT_FOUND':
+        return "Sorry, couldn't find the distance"
+    
+    distance_km = my_dist['distance']['text']
+    duration = my_dist['duration']['text']
+    return f"Distance from {data['destination_addresses'][0]} to {data['origin_addresses'][0]} is {distance_km} and the duration is {duration}"
+
+def google_places_wrapper(query:str) -> str:
+    """ A wrapper around Google Places. 
+        Useful for when you need to find address of some place near property
+        discover addressed from ambiguous text or validate address.
+        Input should be a search query."""
+    places_tool = GooglePlacesTool(api_wrapper=GooglePlacesAPIWrapper(top_k_results=5))
+    res = places_tool.run(query)
+    print(f"Google places result : {res}")
+    return res
+
+def get_info_about_nearby_homes(location:str) -> str:
+    """Tool that uses Zillow api to search for nearby properties given adress of the house. Use case answer on questions related to the properties nearby. Valid params include "location":"location"."""
+    zpid = find_zpid(location)
+    if zpid == "Sorry, could you please give full address":
+        return zpid
+    
+    base_url = "https://zillow-com1.p.rapidapi.com/property"
+    
+    headers = {
+	"X-RapidAPI-Key": "40953cc5afmsh9d09a374a48d7f2p115b37jsnca69ccb7de17",
+	"X-RapidAPI-Host": "zillow-com1.p.rapidapi.com"
+    }
+
+    if zpid is not None:
+        querystring = {"zpid":f"{zpid}"}
+    else:
+        raise Exception("Didn't get zpid")
+        
+
+    # print(f'Search with {querystring}')
+    result = requests.get(base_url, params=querystring, headers=headers)
+    # print(result.json())
+    post_processed = result.json()['nearbyHomes']
+    return post_processed
 
 class Model():
     def __init__(self):
         self.max_length = 16_000
-        find_zpid_tool = StructuredTool.from_function(find_zpid)
-        get_house_property_tool = StructuredTool.from_function(get_house_property)
-        tools = [find_zpid_tool, get_house_property_tool] # Add any tools here
-        llm = ChatOpenAI(temperature=0,openai_api_key="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") 
-        chat_history = MessagesPlaceholder(variable_name="chat_history")
-        memory = ConversationBufferMemory(memory_key="chat_history", return_messages=True)
+        
+        class SearchInput(BaseModel):
+            query: str = Field(description="should be an address in similar to this format 18070 Langlois Rd SPACE 212, Desert Hot Springs, CA 92241")
 
-        self.agent_chain = initialize_agent(
-            tools, 
-            llm, 
-            agent=AgentType.STRUCTURED_CHAT_ZERO_SHOT_REACT_DESCRIPTION, 
-            verbose=True, 
-            memory=memory, 
-            agent_kwargs = {
-                "memory_prompts": [chat_history],
-                "input_variables": ["input", "agent_scratchpad", "chat_history"]
-            }
+        class AdditionalInfo(BaseModel):
+            add: str = Field(description="should be a question regarding information that needed to answer on person question")
+            
+        search_tool = Tool(
+            name="Search Tool",
+            func=get_house_property,
+            description="useful when need to search for info about house, but not about places near it.  The input to this tool should be an address of the house",
+            args_schema = SearchInput
         )
 
+        # find_distance_tool = StructuredTool.from_function(find_distance)
+        find_distance_tool = Tool(
+            name="Find distance tool",
+            func=find_distance,
+            description="useful when need to find distance between two addresses. You may use google_places tool to find address. The input to this tool should be a | separated list of addresses of length two, representing the two addresses you want to find distance between. For example, `13545 Cielo Azul Way, Desert Hot Springs, CA 92240|12105 Palm Dr, Desert Hot Springs, CA 92240` would be the input if you wanted to find distance between 13545 Cielo Azul Way, Desert Hot Springs, CA 92240 and 12105 Palm Dr, Desert Hot Springs, CA 92240.",
+            # args_schema = AdditionalInfo
+        )
 
-    def response(self,user_input):
-        response = self.agent_chain.run(user_input)
-        return response
+        find_nearby_homes = Tool(
+            name = "Find nearby homes",
+            func = get_info_about_nearby_homes,
+            description = "useful when need to search for info about other houses that are listed and located in specific address, but not about places near it.  The input to this tool should be an address of the house"
+        )
+
+        google_places = Tool(
+            name = 'google_places',
+            func = google_places_wrapper,
+            description = """A wrapper around Google Places. 
+        Useful for when you need to find address of some place near property
+        discover addressed from ambiguous text or validate address.
+        Input should be a search query."""
+        )
+        
+
+        
+        tools = [search_tool, google_places, find_distance_tool,find_nearby_homes]
+
+        self.memory = ConversationBufferMemory(memory_key="chat_history")
+        llm = ChatOpenAI(temperature=0.0,openai_api_key="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",max_tokens=512,model="gpt-3.5-turbo-16k")
+
+        def _handle_error(error) -> str:
+            _,though = str(error).split("Could not parse LLM output:")
+            return though
+
+        self.agent_chain = initialize_agent(tools,llm,memory=self.memory,agent="chat-zero-shot-react-description", verbose=True,early_stopping_method="generate",max_iterations=5, handle_parsing_errors=_handle_error)
+
+    def split_messages(self,text):
+        user_messages, ai_messages = [],[]
+        for single_sample in text.split('You:'):
+            if single_sample.strip() != '':
+                if len(single_sample.split("AI:")) == 2:
+                    user_message, ai_message = single_sample.split("AI:")
+                    user_message = user_message.strip()
+                    ai_message = ai_message.strip()
+                    user_messages.append(user_message)
+                    ai_messages.append(ai_message)
+
+
+        return user_messages, ai_messages
+
+    def add_memory(self,user_messages, ai_messages):
+        messages = zip(user_messages,ai_messages)
+        history_length = len(list(messages))
+        i = 0
+        previous_human_messages = []
+        previous_ai_messages = []
+        for user_message,ai_message in zip(user_messages,ai_messages): 
+            if i >= history_length-2:
+                self.memory.chat_memory.add_user_message(user_message)
+                self.memory.chat_memory.add_ai_message(ai_message)
+                previous_human_messages.append(user_message)
+                previous_ai_messages.append(ai_message)
+            i+=1
+        return previous_human_messages, previous_ai_messages
+        
+    def response(self,user_input,message_history):
+        user_messages, ai_messages = self.split_messages(message_history)
+        previous_human_messages, previous_ai_messages = self.add_memory(user_messages, ai_messages)
+        self.clip_context()
+        ai_response = self.agent_chain.run(user_input)
+        return self.refine_ai_response(user_input,ai_response,previous_human_messages, previous_ai_messages)
+
+    def refine_ai_response(self,user_input,rough_ai_response,previous_human_messages, previous_ai_messages):
+        llm = ChatOpenAI(temperature=0.0,openai_api_key="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",max_tokens=1200)
+        messages = [
+                    SystemMessage(
+                        content=f"""The AI is a friendly, helpful and supportive real estate agent named Rick, have a conversation with client, please answer on this client's message.  Don't provide general information If the user message is something like "I am interested in [address]". Do not sound repetative or too much like a servant.
+                Below is the information that you might need to answer the question, use it only when it is related to the user message:
+                {rough_ai_response}"""
+                    )
+                ]
+        for human_message, ai_message in zip(previous_human_messages, previous_ai_messages):
+            messages.append(HumanMessage(content=human_message))
+            messages.append(AIMessage(content=ai_message))
+        messages.append(HumanMessage(content=user_input))
+        refined_response = llm(messages).content
+        
+        return refined_response
+
+    def get_content_length(self):
+        total_length = 0
+        for message in self.memory.chat_memory.messages:
+            total_length += len(message.content)
+        return total_length
+    
+    def clip_context(self):
+        while self.get_content_length() > self.max_length:
+            self.memory.chat_memory.messages.pop(0)
+            self.memory.chat_memory.messages.pop(0)
+        
--- api.py
+++ api.py
@@ -1,71 +1,65 @@
 from typing import Union
 from fastapi import FastAPI
-from ai_model2 import Model
+from ai_model import Model
 from pydantic import BaseModel
 from fastapi import Request
 import openai
-import re
 import requests
+import re
+import os
+from dotenv import load_dotenv
 
-openai.api_key = "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"
+# Load .env file
+load_dotenv()
+
+# Read an environment variable
+os.environ["OPENAI_API_KEY"] =  os.getenv('GPLACES_API_KEY')
+
 app = FastAPI()
-
-class UserMessage(BaseModel):
-    message: str
-    address : str
-
-# @app.get("/")
-# def read_root():
-#     return {"Hello": "World"}
-
-
-# @app.get("/items/{item_id}")
-# def read_item(item_id: int, q: Union[str, None] = None):
-#     return {"item_id": item_id, "q": q}
-
-chatmodel = Model()
-
 
 @app.post('/send_message_to_ai')
 async def send_message_to_ai(request:Request):
+    chatmodel = Model()
     res = await request.json()
-    
     user_message = res['customData']['message']
     address = res['customData']['address']
+    message_history = res['customData']['message_history']
     email = res.get('email')
     phone = res.get('phone')
     user_query = f'{user_message} +  I am interested in {address}'
-    
-    print(user_query)
-    ai_response = chatmodel.response(user_query)
+    ai_response = chatmodel.response(user_query,message_history)
     requests.post('https://services.leadconnectorhq.com/hooks/Cr4I5rLHxAhYI19SvpP6/webhook-trigger/f15fe780-1831-47de-8bfd-9241b8ac626c',json={'bot_response' : ai_response, 'phone':phone,'email':email})
     return {'bot_response' : ai_response}
 
 
-def clip_history(message_history,prompt):
-    max_lenght = 8_000
-    current_length = len(prompt+message_history)
-    if current_length > max_lenght:
-        prompt = prompt
-        need_to_remove = current_length - max_lenght
-        message_history = message_history[need_to_remove:]
-    return message_history
 
-@app.post('/clip_message_history')
-async def clip_message_history(request:Request):
-    res = await request.json()
+## Send to GHL's GPT
+# def clip_history(message_history,prompt):
+#     max_lenght = 8_000
+#     current_length = len(prompt+message_history)
+#     if current_length > max_lenght:
+#         prompt = prompt
+#         need_to_remove = current_length - max_lenght
+#         message_history = message_history[need_to_remove:]
+#     return message_history
+
+# @app.post('/clip_message_history')
+# async def clip_message_history(request:Request):
+#     res = await request.json()
     
-    message_history = res['customData']['message_history']
-    prompt = res['customData']['openai_prompt']
-    print("PROMPT IS",prompt)
-    email = res.get('email')
-    phone = res.get('phone')
-    message_history = clip_history(message_history,prompt)
-    print('Clipped message : ',message_history)
-    requests.post('https://services.leadconnectorhq.com/hooks/Cr4I5rLHxAhYI19SvpP6/webhook-trigger/94a02662-1c53-4f5b-8573-a9f412c8d568',json={'message_history' : message_history, 'phone':phone,'email':email})
+#     message_history = res['customData']['message_history']
+#     prompt = res['customData']['openai_prompt']
+#     print("PROMPT IS",prompt)
+#     email = res.get('email')
+#     phone = res.get('phone')
+#     message_history = clip_history(message_history,prompt)
+#     print('Clipped message : ',message_history)
+#     requests.post('https://services.leadconnectorhq.com/hooks/Cr4I5rLHxAhYI19SvpP6/webhook-trigger/94a02662-1c53-4f5b-8573-a9f412c8d568',json={'prompt':prompt,'message_history' : message_history, 'phone':phone,'email':email})
     # return {'message_history' : message_history}
 
-# system_prompt = '''If the questions is related to specific property information please answer yes else say no. Do not add anything else.
+## was used with custom webhook
+# basic_prompt = '''If the question is related to specific property information or utilities near that property, please answer yes, otherwise say no. Do not add anything else.
+
 # Examples:
 # User: What is the price of the house?
 # AI: Yes
@@ -80,7 +74,22 @@ async def clip_message_history(request:Request):
 # AI: Yes
 
 # User: How can I address you?
-# AI: No'''
+# AI: No
+
+# User: What is the price of [some address]?
+# AI: Yes
+
+# User: I'm interested in 13151 Cuando Way, Desert Hot Springs, CA 92240
+# AI: No
+
+# User: I'm interested in [address]
+# AI: No
+
+# User: How far away is it?
+# AI: Yes
+
+# Use examples to proceed with this:
+# User: '''
 
 # @app.post('/shouldAskForExtraInfo')
 # async def shouldAskForExtraInfo(request:Request):
@@ -89,19 +98,21 @@ async def clip_message_history(request:Request):
 #     print(res)
 #     print('#########')
 #     user_message = res['customData']['message']
+
+#     prompt = basic_prompt + user_message + "\n AI:"
 #     address = res['customData']['address']
-#     email = res['email']
-#     phone = res['phone']
+#     email = res.get('email')
+#     phone = res.get('phone')
 #     print(address)
 #     result = openai.ChatCompletion.create(
 #             model="gpt-3.5-turbo",
 #             max_tokens=256,
 #             temperature = 0,
 #             messages=[
-#                 {"role": "system", "content": f"{system_prompt}"},
-#                 {"role": "user", "content": user_message}]
+#                 {"role": "user", "content": prompt}]
 #             )
 #     answer = result['choices'][0]['message']['content']
+    
 #     if 'yes' in answer.lower():
 #         print('need to ask extra info')
 #         requests.post('https://services.leadconnectorhq.com/hooks/Cr4I5rLHxAhYI19SvpP6/webhook-trigger/08119398-afc3-4d05-9e75-899388717c2b', json = {'message':user_message,'need_extra_info':'True', 'email':email,'phone':phone})
@@ -113,7 +124,7 @@ async def clip_message_history(request:Request):
         
 #         return {'need_extra_info':'False'}
 
-
+## was used with custom webhook
 # @app.post('/get_address')
 # async def get_address(request:Request):
 #     res = await request.json()
@@ -136,33 +147,3 @@ async def clip_message_history(request:Request):
 #         print('Address not found  ',user_message)
 #         requests.post('https://services.leadconnectorhq.com/hooks/Cr4I5rLHxAhYI19SvpP6/webhook-trigger/ddfd4671-13a1-4c55-b370-b7c5fc77dbfe', json = {'message':user_message,'address':'not found', 'email':email,'phone':phone})
 #         return {'address':'not found','status':200}
-
-
-# async def print_request(request):
-#         print(f'request header       : {dict(request.headers.items())}' )
-#         print(f'request query params : {dict(request.query_params.items())}')  
-#         try : 
-#             print(f'request json         : {await request.json()}')
-#         except Exception as err:
-#             # could not parse json
-#             print(f'request body         : {await request.body()}')
-    
-    
-# @app.post("/printREQUEST")
-# async def create_file(request: Request):
-#     try:
-#         await print_request(request)
-#         return {"status": "OK"}
-#     except Exception as err:
-#         print(f'could not print REQUEST: {err}')
-#         return {"status": "ERR"}
-
-
-
-
-# a = {"OpenAI Prompt": "The AI is a friendly, helpful and supportive real estate agent in Seattle named Rick. The main goal of Rick is to first gather necessary information to qualify prospective buyers and sellers of real estate in the Greater Seattle area and then book an appointment once that information is collected. Rick is talking to a client who could be either a seller or a buyer. Rick should first ask any qualifying question from the client to determine the type of client, whether they are a buyer or a seller. Rick may first ask the client, 'What are you looking to do? Do you want to sell or buy?'. Then, Rick should be able to know if the client wants to buy a home or sell their current home based on the client's answer. If the client is a buyer then Rick should ask first, 'What is your ideal buying timeframe?' Rick should ask one question at a time. Rick should then ask the buyer 'What is your budget?' Once all of this information is collected, Rick should then ask the buyer 'Do you have a pre-approval letter?' After this, Rick should ask one last question from the buyer, 'Is there anything else I should know before our meeting?' After all the mentioned questions are done, then Rick should only then book an appointment for that buyer. Rick must not book the appointment for the buyer until he has asked all the questions from the buyer. On the other hand, if the client is wanting to sell their house, then Rick should first ask 'When are you looking to sell your house?' Once this information is collected then, Rick should ask 'Approximately how much is your house worth?' After this, Rick should ask 'How many bedrooms does your house have?' After all that, Rick should ask 'How many bathrooms?' After this, Rick should ask 'What year was your house built?' Then, Rick should ask 'What`s the sq. footage of your home?' Lastly, once all of these questions have been answered then Rick should ask 'Anything else I should know before our meeting?' After all that, Rick should only then book an appointment for that seller. Rick must not book the appointment until the client has answered the relevant questions."}
-
-
-# {'Calendar ID': 'JPaVx6OwfAZzuslGyIbT', 
-#  'OpenAI Prompt': "The AI is a friendly, helpful and supportive real estate agent in Seattle named Rick. The main goal of Rick is to first gather necessary information to qualify prospective buyers and sellers of real estate in the Greater Seattle area and then book an appointment once that information is collected. Rick is talking to a client who could be either a seller or a buyer. Rick should first ask any qualifying question from the client to determine the type of client, whether they are a buyer or a seller. Rick may first ask the client, “What are you looking to do? Do you want to sell or buy?”. Then, Rick should be able to know if the client wants to buy a home or sell their current home based on client's answer. If the client is a buyer then Rick should ask first, “What is your ideal buying timeframe?” Rick should ask one question at a time. Rick should then ask the buyer “What is your budget?” Once all of this information is collected, Rick should then ask the buyer “Do you have a pre-approval letter?” After this, Rick should ask one last question from the buyer, “Is there anything else I should know before our meeting?” After all the mentioned questions are done, then Rick should only then book an appointment for that buyer. Rick must not book the appointment for the buyer until he has asked all the questions from the buyer. On the other hand, if the client is wanting to sell their house, then Rick should first ask “When are you looking to sell your house?” Once this information is collected then, Rick should ask “Approximately how much is your house worth?” After this, Rick should ask “How many bedrooms does your house have?” After all that, Rick should ask “How many bathrooms?” After this, Rick should ask “What year was your house built?” Then, Rick should ask “What’s the sq. footage of your home?” Lastly, once all of these questions have been answered then Rick should ask “Anything else I should know before our meeting?” After all that, Rick should only then book an appointment for that seller. Rick must not book the appointment until the client has answered the relevant questions.",
-#  'Bot Status': 'Bot', 'Conversation': "AI: Hi, Nazar, it's Aaron. Anything I can help you with today? You:  AI:  You:  AI:  You:  AI:  You:  AI:  AI:  You: START AI:  You: Hi You: ", 'Response': '', 'Username': '', 'Message': '', 'Payment': '', 'Linkedin Profile': '', 'Business Category': '', 'Password': '', 'Linkedin': '', 'Select Service': '', 'Number Of Locations': '', 'Services Needed': '', 'Current Email Sequence': '', 'Number Of Location': '', 'OpenAI FAQ': '', 'Do you serve commercial clients?': '', 'What is your current monthly revenue?': '', 'Do you currently have a marketing budget?': '', 'Do you currently serve commercial clients?': '', 'Revenue': '', 'Preferred Types of Clients': '', 'Annual Revenue': '', 'Have you bought a property since we last spoke?': '', 'Are you currently in the market for a property?': '', 'What type of property are you interested in?': '', 'What is your budget?': '', 'Preferred Location?': '', 'What is your timeline for purchasing a property?': '', 'Are you working with another real estate agent?': '', "What are the most important features you're looking for in a property?": '', 'contact_id': 'dhpPDQAGQYYjcaTVH0QU', 'first_name': 'Nazar', 'last_name': 'Andrushko', 'full_name': 'Nazar Andrushko', 'email': 'nazar.andrushko@litslink.com', 'phone': '+380630772038', 'tags': 'qualified,robot reply', 'country': 'CA', 'date_created': '2023-07-24T08:26:00.246Z', 'full_address': '', 'contact_type': 'lead', 'location': {'name': 'Envy', 'address': '1030 Tara Crescent', 'city': 'Parksville', 'state': 'British Columbia', 'country': 'CA', 'postalCode': 'V9P 2W5', 'fullAddress': '1030 Tara Crescent, Parksville British Columbia V9P 2W5', 'id': 'Cr4I5rLHxAhYI19SvpP6'}, 'message': {'type': 3, 'body': 'WoW\n', 'direction': 'inbound'}, 'workflow': {'id': 'c79e54d8-c6d0-4751-bfa9-8e6627f04fab', 'name': 'Test Workfloww'}, 'contact': {'attributionSource': {'sessionSource': 'CRM UI', 'mediumId': None, 'medium': 'manual'}, 'lastAttributionSource': {}}, 'attributionSource': {}, 'customData': {}}
